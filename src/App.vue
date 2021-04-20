aefa84a754731430d20dae10ee42d8758c2eaba3
Chiziii <igwechizy@gmail.com>
2021-04-20 10:06:02 +0100

    Project setup and neccessary updates

---
+++ src/App.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="app">
+    <Card/>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+#app {
+    width: 100%;
+    font-family: 'Roboto', sans-serif;
+}
+
+.cardBody {
+    position: absolute;
+    width: 499px;
+    height: 305px;
+    /* left: 729px; */
+    top: calc(50% - 305px / 2 - 36.5px);
+    background: #131313;
+    border-radius: 20px;
+}
+
+.iconBackground {
+    background-image: url(images/patriciaIcon.png);
+    background-size: cover;
+    position: relative;
+    top: -2.5rem;
+    left: -2rem;
+    background-repeat: no-repeat;
+    height: 21.5rem;
+}
+
+.cardContents {
+    position: relative;
+    left: 5.4rem;
+    top: 5.9rem;
+}
+
+.passwordEye {
+    position: relative;
+    left: 17rem;
+    background: transparent;
+    border: none;
+}
+
+input {
+    border: none;
+    background: transparent;
+    color: rgb(255, 255, 255);
+    font-style: normal;
+    font-weight: normal;
+    font-size: 23px;
+    line-height: 29px;
+    letter-spacing: 0.08em;
+}
+
+input:active {
+    outline: none;
+    border: none;
+}
+
+input:focus {
+    outline: 0;
+}
+
+button:active {
+    outline: none;
+    border: none;
+}
+
+button:focus {
+    outline: 0;
+}
+
+.passwordText {
+    position: relative;
+    top: 5rem;
+}
+
+.passwordAppendage {
+    color: #fff;
+    position: relative;
+    left: 2rem;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 23px;
+    line-height: 29px;
+    letter-spacing: 0.08em;
+}
+
+.openPasswordAppendage {
+    color: #fff;
+    position: relative;
+    left: 3rem;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 23px;
+    line-height: 29px;
+    letter-spacing: 0.08em;
+}
+
+.footerInfo {
+    display: flex;
+    color: #fff;
+    position: relative;
+    top: 9rem;
+}
+
+.footerRightInfo {
+    display: flex;
+    color: #fff;
+    position: relative;
+    left: 16rem;
+}
+
+.validDate {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 7px;
+    line-height: 8px;
+    margin-top: 1rem;
+    margin-right: 1rem;
+    color: #9da8b6;
+}
+
+.cardName {
+    position: absolute;
+    color: #d3dce6;
+}
+
+.ccv {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 7px;
+    line-height: 8px;
+    color: #9da8b6;
+    margin-left: 1rem;
+}
+
+#hiddenPassword {
+    margin-left: 0.8rem;
+}
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,63 @@
+<template>
+ <div class="cardBody">
+        <div class="iconBackground">
+            <div class="cardContents">
+                <div>
+                    <img src="../images/patriciaLogo.svg" alt="logo">
+                    <button @click="showPass = !showPass" class="passwordEye">
+                        <span v-show="!showPass">
+                            <img src="../images/MaskClosed.svg" alt="passwordEye">
+                        </span>
+                        <span v-show="showPass">
+                            <img src="../images/Mask.svg" alt="passwordEye">
+                        </span>
+                    </button>
+                </div>
+
+                <div class="passwordText">
+                    <input type="password" v-model="password" v-show="!showPass" readonly/><span v-show="!showPass"
+                        class="passwordAppendage">9784</span>
+                    <input type="text" v-model="password" v-show="showPass" readonly/><span v-show="showPass"
+                        class="openPasswordAppendage">9784</span>
+                </div>
+
+                <div class="footerInfo">
+                    <p class="cardName">
+                        Cardholder's Name
+                    </p>
+                    <div class="footerRightInfo">
+                        <p class="validDate">
+                            VALID <br> THRU
+                        </p>
+                        <p>
+                            10/21
+                        </p>
+                        <p class="ccv">
+                            <label for="">CCV</label>
+                            <input type="password" id="hiddenPassword" v-model="ccv" v-show="!showCCV" readonly/>
+                            <input type="text" id="hiddenPassword" v-model="ccv" v-show="showCCV" readonly/>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+   data(){
+     return{
+            showPass: false,
+            showCCV: false,
+            password: '9784       9784      9784',
+            ccv: '123'
+        }
+   }
+}
+</script>
+
+<style >
+</style>
